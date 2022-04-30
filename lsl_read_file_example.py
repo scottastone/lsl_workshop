72c8a3e3c8bbf47d5c6cb4a9c435bf1172adeb4b
Scott Stone <scott.allan.stone@gmail.com>
2022-04-29 20:03:45 -0600

    flip y axis

--- lsl_read_file_example.py
+++ lsl_read_file_example.py
@@ -1,12 +1,12 @@
 import liesl
 import matplotlib.pyplot as plt
 
-# Read the file and extract the data
+# Read the file and extract the data 
 file = liesl.XDFFile('mouse.xdf')
 position = file['MousePosition'].time_series
-# Split by X and Y
 x, y = position.T
 
 # Plot the data
 plt.scatter(x, y, marker='.')
+plt.gca().invert_yaxis()
 plt.show()
